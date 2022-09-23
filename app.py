a835c6ff6205bf4a42c371514c936086aec82a37
Corvettemina <mina.h.hanna@gmail.com>
2022-09-23 14:52:27 -0400

    update

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask, render_template, request
-import springApiTest
+from flask import Flask, redirect, url_for, render_template, session, request
+from flask_wtf import FlaskForm
+from wtforms.fields import DateField
+from wtforms.validators import DataRequired
+from wtforms import validators, SubmitField
+from springApiTest import getlist
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '#$%^&*'
+
+
+class InfoForm(FlaskForm):
+    startdate = DateField('Start Date', format='%Y-%m-%d')
+    submit = SubmitField('Submit')
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    listss = springApiTest.getlist()
-    if request.method == 'POST': 
+    form = InfoForm()
+    if form.validate_on_submit():
+        session['startdate'] = form.startdate.data
+        return redirect('select')
+    return render_template('index.html', form=form)
+
+
+@app.route('/select', methods=['GET', 'POST'])
+def select():
+    print('session', session['startdate'])
+    listss = getlist(session['startdate'])
+    if request.method == 'POST':
         print(request.form.getlist('mycheckbox'))
         return 'Done'
-    return render_template('index.html', listss = listss)
+    return render_template('select.html', listss=listss)
--- springApiTest.py
+++ springApiTest.py
@@ -1,7 +1,12 @@
 import platform
+from unittest import mock
 import requests
 import json
 import os
+import datetime
+
+today = datetime.date.today()
+d1 = today.strftime("%Y/%m/%d")
 
 if ("Windows" in platform.platform()):
     path = "C:/Users/Mina Hanna/OneDrive/"
@@ -18,21 +23,33 @@ def getfile_insensitive(path):
             return newpath
 
 
-def getlist():
+def getlist(date=d1):
+    print('date as passed', date)
+    datearr = date.split(" ")
+
+    datetime_object = datetime.datetime.strptime(datearr[2], "%b")
+    month_number = datetime_object.month
+    if (month_number < 10):
+        month_number = "0" + str(month_number)
+
+    newDate = str(datearr[3]) + '-' + str(month_number) + '-' + str(datearr[1])
+    print(str(newDate))
+
     response = requests.get(
-        'http://192.81.219.24:8080/greeting?date=2022-10-16')
+        'http://192.81.219.24:8080/greeting?date=' + str(newDate))
+
     answer = []
     y = json.loads(response.text)
 
     # print(y[0]['standardDoxologies'])
-
+    print(y[0]['date'])
     for i in y[1]:
         if (i != "Ocassion" and i != "Season" and i != "Sunday"):
             if (type(y[1][i]) is list):
                 for l in y[1][i]:
                     # print(l)
                     #print(getfile_insensitive(path + l))
-                    l = l.replace('powerpoints','PowerPoints')
+                    l = l.replace('powerpoints', 'PowerPoints')
                     answer.append(l)
                     pass
             else:
@@ -43,6 +60,8 @@ def getlist():
     return answer
 
 
+'''
 print(platform.platform())
 for i in getlist():
     print(i)
+'''
