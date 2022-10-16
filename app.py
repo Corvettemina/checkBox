a17e2a850f140951d0dd755895971676410765f5
Corvettemina <mina.h.hanna@gmail.com>
2022-10-15 23:45:26 -0400

    update

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ def select():
     spapi = Springapi(session['startdate'])
     # listss = spapi.getlist()["seasonVespersDoxologies"]
     if request.method == 'POST':
+        redirect('/')
         if ((request.form['yes']) == 'yes'):
             spapi.dictionary["vespersPrayerofThanksgiving"] = "PowerPoints/BackBone/PrayerOfThanksgivingBishop.pptx"
 
