82d698a866928b20ed9349af737b51051a19a311
Corvettemina <mina.h.hanna@gmail.com>
2022-10-19 13:44:12 -0400

    update

---
+++ templates/app.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    console.log("hoooo")
+    var input, filter, ul, li, la, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase(); 
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+  
+  
+    for (i = 0; i < li.length; i++) {  
+      la = li[i].getElementsByTagName("label")[0]; 
+      txtValue = la.textContent || la.innerText; 
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {  
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
+  
