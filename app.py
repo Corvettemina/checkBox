e93fc26621e1f934316742da452976851aa85d7c
Corvettemina <mina.h.hanna@gmail.com>
2023-08-09 21:20:15 -0400

    update

--- app.py
+++ app.py
@@ -429,5 +429,5 @@ def myroute():
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    #app.run(host='0.0.0.0')
+    #app.run(debug=True)
+    app.run(host='0.0.0.0')
