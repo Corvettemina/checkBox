5173b508ec24ecc757a4b7ad6f157210bbebfa41
Corvettemina <mina.h.hanna@gmail.com>
2023-08-19 01:59:01 -0400

    update

--- app.py
+++ app.py
@@ -370,6 +370,6 @@ def myroute():
 
 if __name__ == "__main__":
     #`app.run(debug=True)
-    app.run(host='0.0.0.0')
+    #app.run(host='0.0.0.0')
     
-    #app.run(host='0.0.0.0',ssl_context=('/etc/letsencrypt/archive/stmarkapi.com/cert.pem', '/etc/letsencrypt/archive/stmarkapi.com/privkey.pem'))
+    app.run(host='0.0.0.0',ssl_context=('/etc/letsencrypt/archive/stmarkapi.com/cert.pem', '/etc/letsencrypt/archive/stmarkapi.com/privkey.pem'))
