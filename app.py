7d961a1fee5db57cae1ce7eeb2d3348725e160b4
Corvettemina <mina.h.hanna@gmail.com>
2023-08-19 00:46:25 -0400

    update

--- app.py
+++ app.py
@@ -370,5 +370,5 @@ def myroute():
 
 if __name__ == "__main__":
     #`app.run(debug=True)
-    #app.run(host='0.0.0.0')
-    app.run(host='0.0.0.0',ssl_context=('/root/localhost.crt','/root/localhost.key'))
+    app.run(host='0.0.0.0')
+    #app.run(host='0.0.0.0',ssl_context=('/root/localhost.crt','/root/localhost.key'))
