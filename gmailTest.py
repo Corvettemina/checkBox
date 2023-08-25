ede6854c0b7764f1b13e4aad015cfad6b3f8f2f3
Corvettemina <mina.h.hanna@gmail.com>
2023-08-25 19:01:08 -0400

    update

--- gmailTest.py
+++ gmailTest.py
@@ -3,7 +3,6 @@ from __future__ import print_function
 import base64
 from email.message import EmailMessage
 import pickle
-import google.auth
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
