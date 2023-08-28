09adfc9669da563dfbb04a449bda683022b87fc7
Corvettemina <mina.h.hanna@gmail.com>
2023-08-27 20:04:02 -0400

    update

--- gmailTest.py
+++ gmailTest.py
@@ -25,6 +25,7 @@ def create_html_email(date, database):
                 max-width: 600px;
                 margin: 0 auto;
                 padding: 20px;
+                background-color: #5e7b9e;
                 border: 1px solid #e0e0e0;
                 background-image: url('background.jpg');
                 background-size: cover; /* Adjust as needed */
