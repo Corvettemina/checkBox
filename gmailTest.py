a8fecd7dc4eb5d76332be17e1d4e4df46072dbb5
Corvettemina <mina.h.hanna@gmail.com>
2023-08-28 23:40:50 -0400

    update

--- gmailTest.py
+++ gmailTest.py
@@ -33,9 +33,15 @@ def create_html_email(date, database):
                 padding: 20px;
                 background-color: #f5f5f5;
                 border: 1px solid #e0e0e0;
-                background-image: url('background.jpg');
+                background-image: url('path/to/your/image.jpg');
                 background-size: cover; /* Adjust as needed */
                 background-position: center center; /* Adjust as needed */
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                height: 100vh; /* Make the container at least the height of the viewport */
+            
             }}
                 /* Bold titles and increase text size */
             .container h1 {{
@@ -48,10 +54,10 @@ def create_html_email(date, database):
             }}
             .grid-container {{
                 display: flex;
-                flex-direction: column;
-                align-items: center; /* Center horizontally */
+                flex-wrap: wrap;
                 gap: 20px;
-                margin-top: 20px;
+                align-items: center;
+                justify-content: center;
             }}
             .grid-item {{
                 flex: 0 0 calc(50% - 20px); /* Two items per row, accounting for gap */
