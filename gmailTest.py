0ea0fdcdb1a5ae325e360354a2eb48755f24cf2c
Corvettemina <mina.h.hanna@gmail.com>
2023-08-29 21:21:03 -0400

    update

--- gmailTest.py
+++ gmailTest.py
@@ -120,16 +120,17 @@ def create_html_email(date, database):
         html_content += alternate_content
 
     if ("paralexHymns" in database[date]['liturgyOfWord']):
-        alternate_content = f'''
-        <h1><a href="https://stmark-service.web.app/liturgyOfWord?date={date}">Liturgy Of the Word</a></h1>
-        <h2>Paralex Hymns:<br><p>
-        '''
-        html_content += alternate_content
-        for i in database[date]['liturgyOfWord']['paralexHymns']:
-            doxo = i.split("/")[-1]
-            html_content += f'''
-            <br>{doxo}</br>
+        if(len(database[date]['liturgyOfWord']['paralexHymns'])>0):
+            alternate_content = f'''
+            <h1><a href="https://stmark-service.web.app/liturgyOfWord?date={date}">Liturgy Of the Word</a></h1>
+            <h2>Paralex Hymns:<br><p>
             '''
+            html_content += alternate_content
+            for i in database[date]['liturgyOfWord']['paralexHymns']:
+                doxo = i.split("/")[-1]
+                html_content += f'''
+                <br>{doxo}</br>
+                '''
 
     toRender = database[date]['liturgyOfFaithful']['prayerOfReconcilation'][0].split("/")[-1]
     html_content += f'''
