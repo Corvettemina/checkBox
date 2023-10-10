8c345460e687c0bb3d8fcec7cac950c81355e4d7
Corvettemina <mina.h.hanna@gmail.com>
2023-10-09 22:55:33 -0400

    update

--- changeWord.py
+++ changeWord.py
@@ -22,31 +22,32 @@ def insertChange(input_pptx, replaceString):
     slides = [slide for slide in prs.slides]
     shapes = []
     for slide in slides:
-        for shape in slide.shapes:
-            shapes.append(shape)
+        try:
+            for shape in slide.shapes:
+                shapes.append(shape)
+        except:
+            print("EXCEPTION")
 
     replaces = {
         testString: replaceString
     }
-    try:
-        for shape in shapes:
-            for match, replacement in replaces.items():
-                if shape.has_text_frame:
-                    if (shape.text.find(match)) != -1:
-                        text_frame = shape.text_frame
-                        for paragraph in text_frame.paragraphs:
-                            whole_text = "".join(
-                                run.text for run in paragraph.runs)
-                            whole_text = whole_text.replace(
-                                str(match), str(replacement))
-                            for idx, run in enumerate(paragraph.runs):
-                                if idx != 0:
-                                    p = paragraph._p
-                                    p.remove(run._r)
-                            if bool(paragraph.runs):
-                                paragraph.runs[0].text = whole_text
-    except:
-        print("change word error")
+    
+    for shape in shapes:
+        for match, replacement in replaces.items():
+            if shape.has_text_frame:
+                if (shape.text.find(match)) != -1:
+                    text_frame = shape.text_frame
+                    for paragraph in text_frame.paragraphs:
+                        whole_text = "".join(
+                            run.text for run in paragraph.runs)
+                        whole_text = whole_text.replace(
+                            str(match), str(replacement))
+                        for idx, run in enumerate(paragraph.runs):
+                            if idx != 0:
+                                p = paragraph._p
+                                p.remove(run._r)
+                        if bool(paragraph.runs):
+                            paragraph.runs[0].text = whole_text
 
     tempArray = input_pptx.split(".pptx")
     #newPath = tempArray[0] + "today.pptx"
